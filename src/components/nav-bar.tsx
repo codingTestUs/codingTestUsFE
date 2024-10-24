4d802794b9289a4c6856403d50e36bc905c622ff
aidevksh <aidveksh@gmail.com>
2024-10-24 17:36:08 +0900

    login

--- src/components/nav-bar.tsx
+++ src/components/nav-bar.tsx
@@ -17,7 +17,22 @@ import {
   AvatarImage,
 } from "@/components/ui/avatar"
 
+import axios from 'axios';
+
+
 export default function Navbar() {
+
+  const getLogin = async () => {
+    try {
+      const response = await axios.get('http://150.230.251.225/oauth2/authorization/github', {
+        withCredentials: true,
+      });
+      console.log(response.data);
+    } catch (error) {
+
+    }
+  };
+
   const navigate = useNavigate()
   const [position, setPosition] = React.useState("bottom")
   return (
@@ -31,7 +46,7 @@ export default function Navbar() {
           <Link to="/challenges" className="font-medium text-md hover:underline">Problems</Link>
           <Link to="/" className="font-medium text-md hover:underline">Rank</Link>
           <Link to="/blogs" className="font-medium text-md hover:underline">About Us</Link>
-          
+
         </div>
         <div className="flex items-center justify-end flex-1 space-x-4">
           <div className="hidden md:block">
@@ -39,29 +54,29 @@ export default function Navbar() {
           </div>
 
           <DropdownMenu>
-      <DropdownMenuTrigger asChild>
-        <Button variant="outline">
-          Sign In <ChevronDown className="ml-2 h-4 w-4" />
-        </Button>
-      </DropdownMenuTrigger>
-      <DropdownMenuContent className="w-56">
-        <div className="flex justify-between p-2">
-          <DropdownMenuItem className="flex-1 justify-center">
-            <Github className="mr-2 h-4 w-4" />
-            Github
-          </DropdownMenuItem>
-          <DropdownMenuItem className="flex-1 justify-center">
-            <span className="text-lg">G</span> &nbsp;
-            Google
-          </DropdownMenuItem>
-        </div>
-      </DropdownMenuContent>
-    </DropdownMenu>
+            <DropdownMenuTrigger asChild>
+              <Button variant="outline">
+                Sign In <ChevronDown className="ml-2 h-4 w-4" />
+              </Button>
+            </DropdownMenuTrigger>
+            <DropdownMenuContent className="w-56">
+              <div className="flex justify-between p-2">
+                <DropdownMenuItem className="flex-1 justify-center" onClick={getLogin}>
+                  <Github className="mr-2 h-4 w-4" />
+                  Github
+                </DropdownMenuItem>
+                <DropdownMenuItem className="flex-1 justify-center">
+                  <span className="text-lg">G</span> &nbsp;
+                  Google
+                </DropdownMenuItem>
+              </div>
+            </DropdownMenuContent>
+          </DropdownMenu>
 
           {/* <Button variant="outline" onClick={() => navigate('/login')}>Sign In</Button>
           <Button>Sign Up</Button> */}
-          <ModeToggle/>
-            
+          <ModeToggle />
+
         </div>
       </div>
     </nav>
